4c67c5de00bb0079a99c21ab10f7a2307b77cd10
Go088 <tapkharov@gmail.com>
2024-03-29 22:11:36 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,14 @@
 export default function HomePage() {
   return (
     <div>
-      <h2>Home page</h2>
+      <h3>Secure Contact Management Made Simple</h3>
+      <p>
+        Our web application allows users to securely create and store their
+        contacts online. With user authentication, each user can safely access
+        their personalized contact list from anywhere. Whether it&apos;s for
+        personal or professional use, our platform provides a seamless
+        experience for managing contacts, ensuring privacy and convenience.
+      </p>
     </div>
   );
 }
